702b9ff7be52c4248a4e3c2f3a41693bb81dc86f
takutakuma <tischwahrheit@gmail.com>
2018-03-05 18:10:08 +0900

    fix: ログ整備

--- routes/kanmi.js
+++ routes/kanmi.js
@@ -6,7 +6,7 @@ process.on('unhandledRejection', console.dir);
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  console.log(`url:${req.query}`);
+  console.log(`url:${req.query.url}`);
   const results = await lighthouse(req.query.url);
   res.json(results);
 });
