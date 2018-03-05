0c17e3ab32397dc209fe51841da186decf4c17b0
takutakuma <tischwahrheit@gmail.com>
2018-03-05 17:37:20 +0900

    feat: lighthouse.js実行を追加

---
+++ routes/kanmi.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const lighthouse = require('../services/lighthouse');
+var router = express.Router();
+
+process.on('unhandledRejection', console.dir);
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  console.log(`url:${req.query}`);
+  const results = await lighthouse(req.query.url);
+  res.json(results);
+});
+
+module.exports = router;
